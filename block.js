9241ddf81ea1effb4d9a1f869568ebf3e2b98ecb
John Kayne <johnhoganuke2@gmail.com>
2016-08-11 15:10:29 -0400

    Update block.js

--- block.js
+++ block.js
@@ -1,10 +1,12 @@
 class Block {
   var name;
   var textures = [];
+  var position = [];
   
-  constructor(name, textures) {
+  constructor(name, textures, position) {
     this.name = name;
     this.textures = textures;
+    this.position = position;
   }
   
   function add() {
@@ -15,5 +17,12 @@ class Block {
     var rightFace = blockA.insertElement('figure').setAttribute('class', 'right').insertElement('img').setAttribute('src', textures[3]);
     var topFace = blockA.insertElement('figure').setAttribute('class', 'top').insertElement('img').setAttribute('src', textures[4]);
     var bottomFace = blockA.insertElement('figure').setAttribute('class', 'bottom').insertElement('img').setAttribute('src', textures[5]);
+  }
+  function remove() {
+    document.
+  }
+  
+  function get(position) {
+    return World.blocks[position[0]][position[1]][position[2]];
   }
 }
